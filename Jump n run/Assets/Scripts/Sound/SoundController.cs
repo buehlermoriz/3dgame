d6b411bfffcee6632eff8c69baddb8a4a018f994
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2021-04-19 15:25:16 +0200

    add intro scene with sound

---
+++ Jump n run/Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,18 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
